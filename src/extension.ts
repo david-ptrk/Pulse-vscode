ae5116bff6b5962ad7fb36ce8d5f47ad17a3565e
david-ptrk <davidptrk7@gmail.com>
2026-05-23 15:28:42 +0500

    feat: migrate to Language Server Protocol (LSP)

--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,48 @@
-import * as vscode from 'vscode';
-import { PulseCompletionProvider } from "./completionProvider";
-import { PulseHoverProvider } from './hoverProvider';
-import { PulseDefinitionProvider } from './definitionProvider';
-import { PulseDiagnosticsProvider } from './diagnosticsProvider';
-import { PulseOutlineProvider } from './outlineProvider';
-import { PulseSignatureProvider } from './signatureProvider';
-import { PulseAutoImportProvider } from './autoImportProvider';
-import { PulseRenameProvider } from './renameProvider';
-import { PulseFormattingProvider, PulseRangeFormattingProvider } from './formattingProvider';
-import { PulseReferencesProvider } from './referencesProvider';
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log("Pulse language extension activated");
+    const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
     
-    const completionProvider = vscode.languages.registerCompletionItemProvider(
-        { language: 'pulse', scheme: 'file' },
-        new PulseCompletionProvider(),
-        '.'
+    const serverOptions: ServerOptions = {
+        run: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+        },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: { execArgv: ["--nolazy", "--inspect=6009"] },
+        },
+    };
+    
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "pulse" }],
+        synchronize: {
+            fileEvents: vscode.workspace.createFileSystemWatcher("**/*.pul"),
+        },
+    };
+    
+    client = new LanguageClient(
+        "pulseLanguageServer",
+        "Pulse Language Server",
+        serverOptions,
+        clientOptions
     );
     
-    const autoImportProvider = vscode.languages.registerCompletionItemProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseAutoImportProvider()
-    );
-    
-    const hoverProvider = vscode.languages.registerHoverProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseHoverProvider()
-    );
-    
-    const definitionProvider = vscode.languages.registerDefinitionProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseDefinitionProvider()
-    );
-    
-    const outlineProvider = vscode.languages.registerDocumentSymbolProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseOutlineProvider()
-    );
-    
-    const signatureProvider = vscode.languages.registerSignatureHelpProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseSignatureProvider(),
-        "(", ","
-    );
-    
-    const renameProvider = vscode.languages.registerRenameProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseRenameProvider()
-    );
-    
-    const formattingProvider = vscode.languages.registerDocumentFormattingEditProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseFormattingProvider()
-    );
-    
-    const rangeFormattingProvider = vscode.languages.registerDocumentRangeFormattingEditProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseRangeFormattingProvider()
-    );
-    
-    const referencesProvider = vscode.languages.registerReferenceProvider(
-        { language: "pulse", scheme: "file" },
-        new PulseReferencesProvider()
-    );
-    
-    const diagnosticsProvider = new PulseDiagnosticsProvider();
-    diagnosticsProvider.activate(context);
-    
-    context.subscriptions.push(
-        completionProvider,
-        autoImportProvider,
-        hoverProvider,
-        definitionProvider,
-        outlineProvider,
-        signatureProvider,
-        renameProvider,
-        formattingProvider,
-        rangeFormattingProvider,
-        referencesProvider
-    );
+    client.start();
+    context.subscriptions.push(client);
 }
 
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) return undefined;
+    return client.stop();
+}
